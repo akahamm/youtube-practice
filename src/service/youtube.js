9f93d282c4423cab19eb0d0b852544ada3ab5e3d
hammlee <hm@daground.io>
2020-11-29 14:27:25 +0900

    asdf

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,30 +1,35 @@
 /** @format */
 
+import axios from "axios";
+
 class Youtube {
   constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+    this.youtube = axios.create({
+      baseURL: `https://youtube.googleapis.com/youtube/v3`,
+      params: { key: key },
+    });
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=24&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get(`videos`, {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 24,
+      },
+    });
+    return response.data.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get(`search`, {
+      params: {
+        part: "snippet",
+        type: "video",
+        maxResults: 24,
+      },
+    });
+    return response.data.items;
   }
 }
 
